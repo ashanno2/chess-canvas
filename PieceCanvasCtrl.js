4f096e92529d71067f3384a0ee1fe4fd3b63ee26
Aaron Shannon <ashanno2@gmail.com>
2013-09-15 22:28:14 -0400

    globalCompositeOperations update

--- PieceCanvasCtrl.js
+++ PieceCanvasCtrl.js
@@ -1,14 +1,18 @@
 var mod = angular.module("pieceEditor", []);
 	mod.directive("pieceCanvas", function(){
 		return {
-			link: function($scope, element){
-			var canvasContext = element[0].getContext('2d');
+			link: function($scope, $element){
+			var canvas = $element[0];
+			var canvasContext = $element[0].getContext('2d');
 			var isDrawing = false;
 			var prevX;
 			var prevY;
 			var width0 = 256;
 			var height0 = 256;
+
 			$scope.Math = window.Math;
+			canvas.width = width0;
+			canvas.height = height0;
 
 			$scope.scaleCanvas = function(){
                                 canvas.width = width0 * $scope.canvasZoom;
@@ -17,23 +21,24 @@ var mod = angular.module("pieceEditor", []);
 			}
 
 			$scope.clearCanvas = function(){
-				canvasContext.fillStyle = "rgba(255, 255, 255, 1.0)";
-				canvasContext.fillRect(0,0,canvas.width,canvas.height);
+				//canvasContext.fillStyle = "rgba(255, 255, 255, 0)";
+				canvasContext.clearRect(0,0,canvas.width,canvas.height);
 			}
 
 			$scope.updateCanvas = function(){
+				canvasContext.globalCompositeOperation = "source-over";
 				var img = document.getElementById("pieceIMG");
 				canvasContext.drawImage(img,0,0);
 			};
 
-			element.bind('mousedown', function(event){
+			$element.bind('mousedown', function(event){
 				prevX = $scope.Math.floor((event.offsetX)/$scope.canvasZoom);
 				prevY = $scope.Math.floor((event.offsetY)/$scope.canvasZoom);
         			canvasContext.beginPath();
         			isDrawing = true;
 			});
 
-			element.bind('mousemove', function(event){
+			$element.bind('mousemove', function(event){
 				if(isDrawing){
 				currentX = $scope.Math.floor((event.offsetX)/$scope.canvasZoom);
 				currentY = $scope.Math.floor((event.offsetY)/$scope.canvasZoom);
@@ -43,19 +48,20 @@ var mod = angular.module("pieceEditor", []);
 				}
 			});
 
-			element.bind('mouseup', function(event){
+			$element.bind('mouseup', function(event){
 				isDrawing = false;
 			});
 
-			element.bind('mouseleave', function(event){
+			$element.bind('mouseleave', function(event){
 				isDrawing = false;
 			});
 
 			function reset(){
-				element[0].width = element[0].width;
+				$element[0].width = $element[0].width;
 			}
 
 			function draw(pX, pY, cX, cY){
+				canvasContext.globalCompositeOperation = "source-atop";
 				canvasContext.moveTo(pX,pY);
 				canvasContext.lineTo(cX,cY);
 				canvasContext.lineCap = 'round';
